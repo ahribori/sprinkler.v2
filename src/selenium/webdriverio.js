8b7fba585e2149e4b53fd57d90a9cf147a9bf6c4
ahribori <ahribori@gmail.com>
2018-01-21 12:16:52 +0900

    Headless option set only in production mode

--- src/selenium/webdriverio.js
+++ src/selenium/webdriverio.js
@@ -7,21 +7,21 @@ export default {
         const seleniumOptions = {
             desiredCapabilities: {
                 browserName: browserType,
-                chromeOptions: {
+                chromeOptions: process.env.NODE_env === 'production' ? {
                     args: ['headless'],
-                },
+                } : {},
             },
             protocol: process.env.SELENIUM_PROTOCOL || 'http',
             host: process.env.SELENIUM_HOST || '127.0.0.1',
             port: process.env.SELENIUM_PORT || 4444,
         };
         try {
-            log.debug('========================== START ==========================');
+            log.info('============ RUN ============');
             const browser = webdriverio.remote(seleniumOptions);
             await browser.init();
             const results = await actions(browser);
             await browser.end();
-            log.debug('========================== FINISH ==========================');
+            log.info('============ DONE ============');
             return results;
         } catch (e) {
             log.error(e);
