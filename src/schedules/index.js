f4a291ae301f56bc32396c76bc11a94e7d9b9893
Hyun Seung Jeong <ahribori@gmail.com>
2018-01-31 13:31:03 +0900

    Fix dynamic import bug

--- src/schedules/index.js
+++ src/schedules/index.js
@@ -1,11 +1,10 @@
 import fs from 'fs';
 import path from 'path';
 
-const currentPath = path.resolve('src/schedules');
-const scheduleFiles = fs.readdirSync(currentPath);
+const scheduleFiles = fs.readdirSync(__dirname);
 const filter = new RegExp(/(.js)$/);
 scheduleFiles.forEach(file => {
     if (file !== 'index.js' && filter.test(file)) {
-        require(path.join(currentPath, file));
+        require(path.join(__dirname, file));
     }
 });
