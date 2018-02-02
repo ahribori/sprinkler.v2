d3bffc838f03dc13a5826dcfc3dc6cba92bffb91
Hyun Seung Jeong <ahribori@gmail.com>
2018-02-02 15:55:30 +0900

    Add scheduler selector to production mode

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 import kill from 'fkill';
 import seleniumStandalone from '@selenium/seleniumStandalone';
-import PermanentSession from '@selenium/permanentSession';
-import '@schedules';
 
 const { snapshot } = require('process-list');
 
 seleniumStandalone.start(() => {
-    new PermanentSession();
+    require('@runnable');
 });
 
 process.on('SIGINT', async () => {
---
+++ src/runnable/index.js
@@ -0,0 +1,39 @@
+import fs from 'fs';
+import path from 'path';
+import Prompt from 'prompt-checkbox';
+import PermanentSession from '@selenium/permanentSession';
+
+const filePathList = fs.readdirSync(__dirname);
+const filter = new RegExp(/(.js)$/);
+const files = [];
+filePathList.forEach(file => {
+    if (file !== 'index.js' && filter.test(file)) {
+        files.push(file);
+    }
+});
+
+if (process.env.NODE_ENV === 'production') {
+    const prompt = new Prompt({
+        name: 'Check runnable scripts',
+        message: 'Which scripts do you want to execute?',
+        radio: true,
+        choices: files,
+    });
+    prompt.run()
+        .then(answers => {
+            prompt.ui.close();
+            new PermanentSession();
+            answers.forEach(answer => {
+                require(path.join(__dirname, answer));
+            });
+        })
+        .catch(error => {
+            console.error(error);
+        })
+} else {
+    new PermanentSession();
+    files.forEach(file => {
+        require(path.join(__dirname, file));
+    })
+}
+
