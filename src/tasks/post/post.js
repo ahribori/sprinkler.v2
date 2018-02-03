8ca7b68c242b4b3e837fe1c5de2df566690e01a1
ahribori <ahribori@gmail.com>
2018-02-04 00:11:43 +0900

    Add tistory posting method

---
+++ src/tasks/post/post.js
@@ -0,0 +1,24 @@
+/***
+ * 티스토리에 글을 포스팅
+ * @param tistoryUrl
+ * @param title
+ * @param contents
+ * @param tags
+ * @param browser
+ */
+export const postToTistory = async (tistoryUrl, title, contents, tags, browser) => {
+    /**
+     * Need logged in
+     */
+    await browser.url(`${tistoryUrl}/manage/entry/post`);
+    await browser.click('#tx_switchertoggle');
+    await browser.execute((text) => {
+        document.querySelector('#titleBox').value = text;
+    }, title);
+    await browser.execute((text) => {
+        document.querySelector('#tx_canvas_source').value = text;
+    }, contents);
+    await browser.setValue('#tagInput', tags.join(','));
+    await browser.pause(2000);
+    await browser.click('button.btn_comm.btn_save');
+};
