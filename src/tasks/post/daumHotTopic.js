e5bc2d7e4eaea45d019d7acc59f82ae35a3b2677
Hyun Seung Jeong <ahribori@gmail.com>
2018-02-05 17:20:29 +0900

    한글 받침 검사 메소드 추가

--- src/tasks/post/daumHotTopic.js
+++ src/tasks/post/daumHotTopic.js
@@ -2,6 +2,14 @@ import fs from 'fs';
 import path from 'path';
 import log from '@logger';
 
+const makeTitle = (title) => {
+    const hasPhoneme = (a) => {
+        const r = (a.charCodeAt(0) - parseInt('0xac00',16)) % 28;
+        const t = String.fromCharCode(r + parseInt('0x11A8') -1);
+        return t !== 'ᆧ';
+    };
+};
+
 /**
  * 검색어, 관련검색어, 인물 프로필, 뉴스 오브젝트를 인자로 받아
  * 포스팅할 html 문서를 생성
