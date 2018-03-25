a7ebe187eb63260a24e202a45c66f677dbca4289
ahribori <ahribori@gmail.com>
2018-03-25 11:09:27 +0900

    Kill chromedriver when shutdown

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
+import kill from 'fkill';
+
+const psList = require('ps-list');
+
 require('./modules');
+
+process.on('SIGINT', async () => {
+    try {
+        const killList = [];
+        const chromeRegex = new RegExp(/chromedriver/);
+        const processes = await psList();
+        processes.forEach(process => {
+            chromeRegex.test(process.name) && killList.push(process.pid);
+        });
+        await kill(killList, {
+            force: true
+        });
+    } catch (e) {
+        process.exit();
+    }
+    process.exit();
+});
