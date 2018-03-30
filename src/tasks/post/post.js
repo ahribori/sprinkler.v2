642940d376f1220d6f5e714610636e4b14908289
ahribori <ahribori@gmail.com>
2018-03-30 12:50:21 +0900

    postToTistory logging

--- src/tasks/post/post.js
+++ src/tasks/post/post.js
@@ -1,3 +1,4 @@
+import log from '@logger';
 /***
  * 티스토리에 글을 포스팅
  * @param tistoryUrl
@@ -12,6 +13,7 @@ export const postToTistory = async (tistoryUrl, title, contents, tags, browser) 
      */
     await browser.url(`${tistoryUrl}/manage/entry/post`);
     await browser.click('#tx_switchertoggle');
+    log.info('[post.postToTistory]', 'click #tx_switchertoggle');
     await browser.execute((text) => {
         document.querySelector('#titleBox').value = text;
     }, title);
@@ -20,9 +22,12 @@ export const postToTistory = async (tistoryUrl, title, contents, tags, browser) 
     }, contents);
     await browser.pause(3000);
     await browser.setValue('#tagInput', tags.join(','));
+    log.info('[post.postToTistory]', 'setValue #tagInput');
     await browser.pause(3000);
     await browser.click('#visibilityOption .btn_tab.btn_public');
+    log.info('[post.postToTistory]', 'click #visibilityOption .btn_tab.btn_public');
     await browser.pause(3000);
     await browser.click('button.btn_comm.btn_save');
+    log.info('[post.postToTistory]', 'click button.btn_comm.btn_save');
     await browser.pause(5000);
 };
