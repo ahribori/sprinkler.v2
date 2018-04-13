9367b33ad4e38e15c0cd3a8462413d1de17b35f1
ahribori <ahribori@gmail.com>
2018-04-13 11:06:50 +0900

    네이버 검색어만 추출하도록 변경

--- src/modules/realtime-hot-issue.js
+++ src/modules/realtime-hot-issue.js
@@ -6,6 +6,8 @@ import cron from '@cron';
 import { run } from '@selenium';
 import {
     getHotTopicList,
+} from '../tasks/crawling/naverHotTopic';
+import {
     searchByKeyword,
 } from '../tasks/crawling/daumHotTopic';
 import { buildRTHIPost } from '../tasks/post/daumHotTopic';
@@ -22,11 +24,11 @@ const job = new cron('0 0,30 6-23 * * *', () => {
     const randomTimeoutMinutes = Math.round(Math.random() * (minutesTimeoutRange - 1));
     const randomTimeoutSecond = Math.round(Math.random() * (secondTimeoutRange - 1));
     const timeout = ((randomTimeoutMinutes * 60) + (randomTimeoutSecond)) * 1000;
-    log.info(`RTHI 포스팅 ${randomTimeoutMinutes}분 ${randomTimeoutSecond}초 후에 실행.`);
+    log.info('[rthi]', `${randomTimeoutMinutes}분 ${randomTimeoutSecond}초 후에 실행.`);
     setTimeout(() => {
         const { rthi } = config.tistory;
         run(async (browser) => {
-            log.info(`RTHI 포스팅 시작`);
+            log.info('[rthi]', `포스팅 시작`);
             const topicList = await getHotTopicList(browser);
             const successLogPath = path.resolve('logs/success.log');
             fs.existsSync(successLogPath) || fs.writeFileSync(successLogPath, JSON.stringify({}), 'utf-8');
@@ -38,24 +40,28 @@ const job = new cron('0 0,30 6-23 * * *', () => {
                 if (successLogTree[topicList[i]] === undefined) {
                     // TODO 포스팅
                     KEYWORD = topicList[i];
-                    log.info(`검색어 "${KEYWORD}"로 포스팅을 시작합니다`);
+                    log.info('[rthi]', `검색어 "${KEYWORD}"로 포스팅을 시작합니다`);
                     successLogTree[topicList[i]] = 1;
                     break;
                 }
             }
             if (!KEYWORD) {
-                log.info('검색어 없음');
+                log.info('[rthi]', '검색어 없음');
                 return;
             }
             fs.writeFileSync(successLogPath, JSON.stringify(successLogTree, null, '\t'), 'utf-8');
 
+            log.info('[rthi]', `searchByKeyword start`);
             const result = await searchByKeyword(KEYWORD, browser);
+            log.info('[rthi]', `buildPost`);
             const post = buildRTHIPost(KEYWORD, result.relatedKeywords, result.profile, result.news, result.summary);
             const postDirPath = path.resolve('logs/post');
             fs.existsSync(postDirPath) || fs.mkdirSync(postDirPath);
             // fs.writeFileSync(path.join(postDirPath, `${Date.now()}_${KEYWORD}.html`), post.contents, 'utf-8');
             await closePopup(browser);
+            log.info('[rthi]', `login`);
             await login(rthi.id, rthi.pw, browser);
+            log.info('[rthi]', `postToTistory`);
             await postToTistory(
                 rthi.domain,
                 post.title,
