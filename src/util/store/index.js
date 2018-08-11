6a2d262cc2ff6cf72243cb9519113e74c0e7c994
ahribori <ahribori@gmail.com>
2018-08-11 11:34:39 +0900

    JSON file store 유틸 모듈 작성

---
+++ src/util/store/index.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+
+const STORE_DIR_PATH = path.resolve('store');
+fs.existsSync(STORE_DIR_PATH) || fs.mkdirSync(STORE_DIR_PATH);
+
+export const save = (name = 'default', object = {}) => {
+    const STORE_PATH = path.join(STORE_DIR_PATH, `${name}.json`);
+    const exist = fs.existsSync(STORE_PATH);
+    if (!exist) {
+        fs.writeFileSync(STORE_PATH, JSON.stringify(object, null, '\t'), 'utf-8');
+    }
+};
+
+export const load = (name) => {
+    const STORE_PATH = path.join(STORE_DIR_PATH, `${name}.json`);
+    const exist = fs.existsSync(STORE_PATH);
+    if (!exist) {
+        return null;
+    }
+    return JSON.parse(fs.readFileSync(STORE_PATH));
+};
