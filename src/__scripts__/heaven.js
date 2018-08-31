47f0efcb39bda01bb5a8d3e2ebbfc4301d8320c5
ahribori <ahribori@gmail.com>
2018-09-01 01:12:42 +0900

    heaven 시작 시간 로깅

--- src/__scripts__/heaven.js
+++ src/__scripts__/heaven.js
@@ -1,4 +1,5 @@
 import cron from '@cron';
+import log from '@logger';
 import { run } from '../selenium';
 import { heaven } from '../modules/post/heaven';
 
@@ -8,6 +9,7 @@ const job = new cron('0 0,30 0,1,2,19-23 * * *', () => {
     const randomTimeoutMinutes = Math.round(Math.random() * (minutesTimeoutRange - 1));
     const randomTimeoutSecond = Math.round(Math.random() * (secondTimeoutRange - 1));
     const timeout = ((randomTimeoutMinutes * 60) + (randomTimeoutSecond)) * 1000;
+    log.info('[heaven]', `${randomTimeoutMinutes}분 ${randomTimeoutSecond}초 후에 실행.`);
     setTimeout(() => {
         run(async browser => {
             await heaven(browser);
