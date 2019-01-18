8661c1af685fee794e1e2fb6e73962ca95ffdf67
ahribori <ahribori@gmail.com>
2019-01-19 00:07:56 +0900

    scripts 재귀적 require 구현

---
+++ src/config/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const yaml = require('js-yaml');
+
+let doc = null;
+
+try {
+  doc = yaml.safeLoad(fs.readFileSync(path.resolve('config.yaml'), 'utf8'));
+  if (!doc) {
+    doc = null;
+  }
+} catch (e) {
+  logger.error(e.message);
+}
+
+if (!doc) {
+  throw new Error('Invalid configurations. (check config.yaml)');
+}
+
+module.exports = doc;
