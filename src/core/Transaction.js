e29951c3090057fe6630d6e70ac525ae09a9f3b7
ahribori <ahribori@gmail.com>
2019-01-21 00:48:30 +0900

    Chrome & Firefox headless option 추가

--- src/core/Transaction.js
+++ src/core/Transaction.js
@@ -5,23 +5,43 @@ import TransactionManager from './TransactionManager';
 const { selenium } = config;
 const { protocol, host, port, browser, logLevel } = selenium;
 
-const defaultOption = {
-  logLevel: logLevel || 'info',
-  capabilities: {
-    browserName: browser || 'chrome',
-  },
-  protocol: protocol || 'http',
-  host: host || '127.0.0.1',
-  port: port || 4444,
-};
-
-const customOption = {
-  enableAutoDeleteSession: true,
-};
-
 class Transaction {
+  getCapabilities = ({ headless = true }) => ({
+    chrome: {
+      browserName: 'chrome',
+      'goog:chromeOptions': {
+        args: headless ? ['--headless'] : ['--window-size=1600,900'],
+      },
+    },
+    firefox: {
+      browserName: 'firefox',
+      'moz:firefoxOptions': {
+        args: headless ? ['--headless'] : [],
+      },
+    },
+  });
+
+  customOption = {
+    enableAutoDeleteSession: true,
+  };
+
   constructor(options) {
-    this.options = Object.assign({}, defaultOption, customOption, options);
+    this.options = Object.assign(
+      {},
+      {
+        logLevel: logLevel || 'info',
+        capabilities:
+          this.getCapabilities(options)[options.browser] ||
+          this.getCapabilities(options)[browser] ||
+          this.getCapabilities(options)['chrome'],
+        protocol: protocol || 'http',
+        host: host || '127.0.0.1',
+        port: port || 4444,
+      },
+      this.customOption,
+      options,
+    );
+    console.log(JSON.stringify(this.options, null, 2));
   }
 
   run() {
